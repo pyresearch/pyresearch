bc45b3ac9c9fb7e01be37ead94494b77e8b26a79
Noorkhokhar <34125851+noorkhokhar99@users.noreply.github.com>
2023-02-21 22:04:55 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup
+
+setup(
+    name='Pyresearch',
+    packages=['Pyresearch'],
+    version='1.5.6',
+    license='MIT',
+    description='Computer Vision Helping Library',
+    author='Pyresearch',
+    author_email='pyresearch0@gmail.com',
+    url='https://github.com/pyresearch/pyresearch.git',
+    keywords=['ComputerVision', 'HandTracking', 'FaceTracking', 'PoseEstimation'],
+    install_requires=[
+        'opencv-python',
+        'numpy'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',  # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
